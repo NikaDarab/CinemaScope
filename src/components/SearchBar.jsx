94fac519774fcf098b416cae2c806fc7fee05c01
Nika Darab <nikadarab@Nikas-Air.attlocal.net>
2023-03-28 02:14:39 -0500

    search bar styling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import "../styles/SearchBar.css";
 
 const SearchBar = ({ query, setQuery, handleSearch }) => (
-  <>
+  <div className="search-bar">
     <input
+      className="search-bar__input"
       type="text"
       defaultValue={query.title}
       onChange={(e) => setQuery({ ...query, title: e.target.value })}
+      placeholder="Enter movie title"
     />
     <input
+      className="search-bar__input"
       type="text"
       value={query.year}
       onChange={(e) => setQuery({ ...query, year: e.target.value })}
+      placeholder="Enter year (optional)"
     />
-    <button onClick={() => handleSearch(query)}>Search</button>
-    <h1>Search Results</h1>
-  </>
+    <button className="search-bar__button" onClick={() => handleSearch(query)}>
+      Search
+    </button>
+  </div>
 );
 
 export default SearchBar;
