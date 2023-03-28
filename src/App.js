6c2836930554b9adc098f22969e41048cdb6dc1b
Nika Darab <nikadarab@Nikas-Air.attlocal.net>
2023-03-27 23:09:34 -0500

    App.js is refactored

--- src/App.js
+++ src/App.js
@@ -1,68 +1,59 @@
 import "./App.css";
 import { useState } from "react";
-import axios from "axios";
-import { trimTitle } from "./utils/helper";
+import { fetchResults, fetchResult } from "./utils/api";
+import Modal from "./components/Modal";
+import SearchBar from "./components/SearchBar";
+import Results from "./components/Results";
 
 function App() {
   const [data, setData] = useState(null);
   const [movie, setMovie] = useState(null);
+  const [showModal, setShowModal] = useState(false);
   const [error, setError] = useState(null);
   const [query, setQuery] = useState({
     title: "",
     year: "",
   });
 
-  const fetchResults = async (query) => {
-    const trimmedTitle = trimTitle(query.title);
-    await axios
-      .get(
-        `http://www.omdbapi.com/?apikey=e0a78b47&s=${trimmedTitle}&y=${query.year}`
-      )
-      .then((res) => {
-        setData(res.data.Search);
-      })
-      .catch((err) => {
-        setError(err);
-      });
+  const handleSearch = async (query) => {
+    try {
+      const results = await fetchResults(query);
+      setData(results);
+    } catch (error) {
+      setError(error.message);
+    }
   };
 
-  const fetchResult = async (IMDbID) => {
-    await axios
-      .get(`http://www.omdbapi.com/?apikey=e0a78b47&i=${IMDbID}`)
-      .then((res) => {
-        setMovie(res.data);
-      })
-      .catch((err) => {
-        setError(err);
-      });
+  const handleSelect = async (imdbID) => {
+    try {
+      const result = await fetchResult(imdbID);
+      setMovie(result);
+      setShowModal(true);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const setMovieDetails = (imdbID) => {
+    handleSelect(imdbID);
+    setShowModal(true);
   };
 
   return (
     <div className="App">
-      <input
-        type="text"
-        defaultValue={query.title}
-        onChange={(e) => setQuery({ ...query, title: e.target.value })}
+      <SearchBar
+        query={query}
+        setQuery={setQuery}
+        handleSearch={handleSearch}
       />
-      <input
-        type="text"
-        value={query.year}
-        onChange={(e) => setQuery({ ...query, year: e.target.value })}
-      />
-      <button onClick={() => fetchResults(query)}>Search</button>
-      <h1>Search Results</h1>
-      <div>
-        {error && <p>{error}</p>}
-        {data?.map((result) => (
-          <button
-            key={result.imdbID}
-            onClick={() => fetchResult(result.imdbID)}
-            style={{ border: "none", backgroundColor: "transparent" }}
-          >
-            {result.Title}
-          </button>
-        ))}
-      </div>
+      <Results error={error} data={data} setMovieDetails={setMovieDetails} />
+      {showModal && (
+        <Modal
+          movie={movie}
+          showModal={showModal}
+          setShowModal={setShowModal}
+        />
+      )}
     </div>
   );
 }
