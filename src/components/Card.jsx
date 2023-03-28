a4ebdb48aca81e6049f26be3fea09b1933e69f8f
Nika Darab <nikadarab@Nikas-Air.attlocal.net>
2023-03-28 02:15:29 -0500

    card styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/Modal.css";
+import "../styles/Card.css";
 import { useOutsideClickHandler } from "../utils/hooks";
 
 const Card = ({ movie, showModal, setShowModal }) => {
@@ -8,33 +8,42 @@ const Card = ({ movie, showModal, setShowModal }) => {
   return (
     <>
       {showModal && (
-        <div className="modal">
+        <div className="modal-overlay">
           <div className="modal-content" ref={ref}>
-            <button
-              className="close"
-              onClick={() => setShowModal(false)}
-              style={{ border: "none", backgroundColor: "transparent" }}
-            >
-              &times;
-            </button>
-            {movie && (
-              <>
-                <h1>{movie.Title}</h1>
-                <img src={movie.Poster} alt={movie.Title} />
-                <p>{movie.Plot}</p>
-                <p>Director:{movie.Director}</p>
-                <p>Genre:{movie.Genre}</p>
-                <p>Release Date:{movie.Released}</p>
-                <p>IMDb Rating: {movie.imdbRating}</p>
-                <p>
-                  Rotten Tomatoes:
-                  {
-                    movie.Ratings.find((m) => m.Source === "Rotten Tomatoes")
-                      .Value
-                  }
-                </p>
-              </>
-            )}
+          {movie && (
+  <>
+    <h2 className="movie-title">{movie.Title}</h2>
+    <img className="movie-poster" src={movie.Poster} alt={movie.Title} />
+    <p className="movie-plot">{movie.Plot}</p>
+    <div className="movie-details">
+      <div className="movie-detail">
+        <span className="detail-label">Director:</span>
+        <span className="detail-value">{movie.Director}</span>
+      </div>
+      <div className="movie-detail">
+        <span className="detail-label">Genre:</span>
+        <span className="detail-value">{movie.Genre}</span>
+      </div>
+      <div className="movie-detail">
+        <span className="detail-label">Release Date:</span>
+        <span className="detail-value">{movie.Released}</span>
+      </div>
+      <div className="movie-detail">
+        <span className="detail-label">IMDb Rating:</span>
+        <span className="detail-value">{movie.imdbRating}</span>
+      </div>
+      {movie.Ratings.find((m) => m?.Source === "Rotten Tomatoes") ? (
+        <div className="movie-detail">
+          <span className="detail-label">Rotten Tomatoes:</span>
+          <span className="detail-value">
+            {movie.Ratings.find((m) => m?.Source === "Rotten Tomatoes")?.Value}
+          </span>
+        </div>
+      ) : null}
+    </div>
+  </>
+)}
+
           </div>
         </div>
       )}
