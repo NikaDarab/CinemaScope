bdad110771ff272048890205b456eb22e49e8b7e
Nika Darab <nikadarab@Nikas-Air.attlocal.net>
2023-03-28 20:33:12 -0500

    search button is disabeled if title is less than 3 characters long

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,27 +2,37 @@ import React from "react";
 import { string, func, shape } from "prop-types";
 import "../styles/SearchBar.css";
 
-const SearchBar = ({ query, setQuery, handleSearch }) => (
-  <div className="search-bar">
-    <input
-      className="search-bar__input"
-      type="text"
-      defaultValue={query.title}
-      onChange={(e) => setQuery({ ...query, title: e.target.value })}
-      placeholder="Enter movie title"
-    />
-    <input
-      className="search-bar__input"
-      type="text"
-      value={query.year}
-      onChange={(e) => setQuery({ ...query, year: e.target.value })}
-      placeholder="Enter year (optional)"
-    />
-    <button className="search-bar__button" onClick={() => handleSearch(query)}>
-      Search
-    </button>
-  </div>
-);
+const SearchBar = ({ query, setQuery, handleSearch }) => {
+  const isDisabled = query.title.length < 3;
+
+  return (
+    <div className="search-bar">
+      <input
+        className="search-bar__input"
+        type="text"
+        defaultValue={query.title}
+        onChange={(e) => setQuery({ ...query, title: e.target.value })}
+        placeholder="Enter movie title"
+      />
+      <input
+        className="search-bar__input"
+        type="text"
+        value={query.year}
+        onChange={(e) => setQuery({ ...query, year: e.target.value })}
+        placeholder="Enter year (optional)"
+      />
+      <button
+        className={`search-bar__button ${
+          isDisabled ? "search-bar__button--disabled" : ""
+        }`}
+        onClick={() => handleSearch(query)}
+        disabled={isDisabled}
+      >
+        Search
+      </button>
+    </div>
+  );
+};
 
 SearchBar.propTypes = {
   query: shape({
