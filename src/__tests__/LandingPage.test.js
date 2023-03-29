597697ced01bcc0d1010ab092b75e6bb3e3145b3
Nika Darab <nikadarab@Nikas-Air.attlocal.net>
2023-03-29 05:42:02 -0500

    updated LandingPage test

--- src/__tests__/LandingPage.test.js
+++ src/__tests__/LandingPage.test.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen
+} from "@testing-library/react";
 import LandingPage from "../components/LandingPage";
 
 describe("LandingPage", () => {
-  it("should render welcome message and description", () => {
-    render(<LandingPage />);
-    const welcomeMessage = screen.getByText("Welcome to Cinema Scope!");
-    const description = screen.getByText(
-      "Search for your favorite movies and discover new ones."
-    );
-    expect(welcomeMessage).toBeInTheDocument();
-    expect(description).toBeInTheDocument();
-  });
-});
+      it("should render welcome message and description", () => {
+          const landingText =
+            '<div><h1>Welcome to Cinema Scope!</h1><p>Search for your favorite movies and discover new ones.</p></div>';
+          render( < LandingPage landingText = {
+              landingText
+            }
+            />);
+            const welcomeMessage = screen.getByText("Welcome to Cinema Scope!");
+            const description = screen.getByText(
+              "Search for your favorite movies and discover new ones."
+            ); expect(welcomeMessage).toBeInTheDocument(); expect(description).toBeInTheDocument();
+          });
+      });
